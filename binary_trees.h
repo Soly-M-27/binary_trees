41963319ce5a8048d7f03a53d9eec609b1f548ad
Soly-M-27 <solymar227@icloud.com>
2022-03-22 03:47:28 +0000

    Adding updated .h library

--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stddef.h>
+#include <stdio.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -28,3 +29,5 @@ typedef struct binary_tree_s heap_t;
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 void binary_tree_print(const binary_tree_t *);
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
