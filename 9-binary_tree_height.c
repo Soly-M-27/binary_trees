aeed46a56bfc55e6ea8adc600638d33a51d7e03c
Soly-M-27 <solymar227@icloud.com>
2022-03-23 05:48:01 +0000

    fixed logical operators

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,11 +12,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left != NULL && tree->right != NULL)
+	if (tree->left && tree->right)
 		return (2);
-	if (tree->left == NULL && tree->right != NULL)
+	if (tree->left == NULL && tree->right)
 		return (1);
-	if (tree->left != NULL && tree->right == NULL)
+	if (tree->left && tree->right == NULL)
 		return (1);
 	return (0);
 }
