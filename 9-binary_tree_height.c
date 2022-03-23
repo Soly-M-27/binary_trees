e7993d0cf259a1e1212cd4bb02a51f54312c8875
Soly-M-27 <solymar227@icloud.com>
2022-03-23 05:43:34 +0000

    Removed basecase

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,14 +12,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if (tree != NULL)
-	{
-		if (tree->left != NULL && tree->right != NULL)
-			return (2);
-		if (tree->left == NULL && tree->right != NULL)
-			return (1);
-		if (tree->left != NULL && tree->right == NULL)
-			return (1);
-	}
+	if (tree->left != NULL && tree->right != NULL)
+		return (2);
+	if (tree->left == NULL && tree->right != NULL)
+		return (1);
+	if (tree->left != NULL && tree->right == NULL)
+		return (1);
 	return (0);
 }
