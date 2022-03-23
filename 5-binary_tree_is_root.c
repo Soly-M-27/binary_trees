39090d7f3ca8fb41380c1278f4c55020b08a938c
Soly-M-27 <solymar227@icloud.com>
2022-03-23 05:22:55 +0000

    Fixed return statement

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -13,6 +13,8 @@ int binary_tree_is_root(const binary_tree_t *node)
 	if (node == NULL)
 		return (0);
 
-	if (node->right != NULL || node->left != NULL)
+	if (node->parent == NULL)
 		return (1);
+
+	return (0);
 }
