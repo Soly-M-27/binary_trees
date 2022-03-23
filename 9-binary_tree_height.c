6c61dcae536a5facc6b640a5ce2f2ca39541f1d6
Soly-M-27 <solymar227@icloud.com>
2022-03-23 05:42:05 +0000

    Hardcoded returns instead of variables

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,18 +9,17 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t x = 0;
-
 	if (tree == NULL)
 		return (0);
 
 	if (tree != NULL)
 	{
-		if (tree->right != NULL)
-			x += 1;
-		if (tree->left != NULL)
-			x += 1;
+		if (tree->left != NULL && tree->right != NULL)
+			return (2);
+		if (tree->left == NULL && tree->right != NULL)
+			return (1);
+		if (tree->left != NULL && tree->right == NULL)
+			return (1);
 	}
-
-	return (x);
+	return (0);
 }
