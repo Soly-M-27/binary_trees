76b475213b5002aeed4a7daa478fff36f6400f95
Soly-M-27 <solymar227@icloud.com>
2022-03-24 01:30:38 +0000

    updating .h library

--- binary_trees.h
+++ binary_trees.h
@@ -1,3 +1,6 @@
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
+
 #include <stdlib.h>
 #include <stddef.h>
 #include <stdio.h>
@@ -61,3 +64,5 @@ int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
 
 int binary_tree_is_perfect(const binary_tree_t *tree);
+
+#endif /* _BINARY_TREES_H_ */
