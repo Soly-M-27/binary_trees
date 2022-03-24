84018b9fcd930a0fa80cdec9c45b14ec13729c7f
Soly-M-27 <solymar227@icloud.com>
2022-03-24 00:54:51 +0000

    Fixed return statement

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -7,7 +7,7 @@
  * Return: If tree is NULL, your function must return 0
  */
 
-int binary_tree_is_full(const binary_tree_t *tree)
+int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
@@ -16,7 +16,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (1);
 
 	if (binary_tree_height(tree->right) == binary_tree_height(tree->left))
-		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+		return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
 
 	return (0);
 }
